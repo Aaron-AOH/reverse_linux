c1305b40c0a2a09c5483426c5e806d5737dda6fa
Aaron-AOH <118374763+Aaron-AOH@users.noreply.github.com>
2023-02-27 09:49:45 +0100

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -7,7 +7,7 @@ os.system('sudo apt-get install python2')
 os.system('sudo apt-get install python3')
 
 os.system('clear')
-print("Si la instalacion ha sido exitosa, deberia estar viendo [apache2 y python3] en pantalla con la version instalada")
+print("Si la instalacion ha sido exitosa, deberia estar viendo [apache2 y python3] en pantalla con la version instalada"\n)
 os.system('apt list apache2 python3')
 
 
